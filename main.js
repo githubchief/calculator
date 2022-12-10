031eb3247e74787ea3b8ca6b18354f0042074318
githubchief <ranganathanshams@gmail.com>
2022-12-10 23:36:32 +0530

    add display and calc function to num and symbols

--- main.js
+++ main.js
@@ -37,3 +37,68 @@ function operate(op,num1, num2) {
             return mod(num1,num2);
     }
 }
+
+//store input,output dom element
+let input=document.getElementById("input");
+let output=document.getElementById("output");
+
+//get the array of digit elements
+let number=document.querySelectorAll(".digit");
+
+//variable to store the input number
+let num1="";
+let num2="";
+let currentInput="";
+//variable to store operator
+let op="";
+
+//for each element in the array of digits invoke getInput
+number.forEach(num => {
+    num.addEventListener("click", getInput1)
+});
+ 
+//function to get the inputs and store it in a separate variable
+function getInput1(e){
+        let temp=e.target;
+        currentInput=currentInput+temp.value;
+        displayInput(currentInput);
+}
+
+//for each element in the operator array get the operator and store it in the op based on the click
+let operator=document.querySelectorAll(".operation");
+operator.forEach(oper => {
+    oper.addEventListener("click", getOperator)
+});
+
+function getOperator(ev) {
+    let temp=ev.target;
+    op=temp.value;
+    num1=currentInput;
+    currentInput="";
+    //when a operator is received, collect the digits to num2;
+    number.forEach(element => {
+        element.addEventListener("click", (e)=> getInput2(e,op))
+    });
+}
+
+function getInput2(e,op){
+
+    currentInput="";
+    let temp=e.target;
+    currentInput=currentInput+temp.value;
+    displayInput(currentInput);
+    num2=currentInput;
+    //once we have num1 and num2 calculate result
+    result=operate(op,parseInt(num1), parseInt(num2));
+    displayOutput(result);
+}
+
+
+function displayInput(e) {
+    //    input.textContent=input.textContent+e;
+    input.innerHTML=e;
+}
+
+function displayOutput(e) {
+    output.innerHTML=e;
+}
