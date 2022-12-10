92a43d6ec36845673f9eb2ee9175499aa40b562a
githubchief <ranganathanshams@gmail.com>
2022-12-10 09:04:11 +0530

    add math functions and check in console

---
+++ main.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    return num1+num2;
+}
+
+function sub(num1, num2) {
+    return num1-num2;
+}
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+
+function divide(num1, num2) {
+    return num1/num2;
+}
+
+function mod(num1, num2) {
+    return num1%num2;
+}
