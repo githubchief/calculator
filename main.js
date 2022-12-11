e9baf59e013bf616e2ec8339938f175735d70a8d
githubchief <ranganathanshams@gmail.com>
2022-12-11 18:11:48 +0530

    add display function and result variable for op

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 //function to add two numbers
 function add(num1, num2) {
-    return output.innerHTML=(num1+num2);
+    return (num1+num2);
 }
 
 //function to subtract two numbers
 function sub(num1, num2) {
-    return output.innerHTML=num1-num2;
+    return (num1-num2);
 }
 
 //function to multiply two numbers
 function multiply(num1, num2) {
-    return output.innerHTML=num1*num2;
+    return (num1*num2);
 }
 
 //function to divide two numbers
 function divide(num1, num2) {
-    return output.innerHTML=num1/num2;
+    return (num1/num2);
 }
 
 //function to find reminder of two numbers
 function mod(num1, num2) {
-    return output.innerHTML=num1%num2;
+    return (num1%num2);
 }
 
 function operate(op,num1, num2) {
@@ -92,12 +92,20 @@ function getInput2(e,op){
     num2=currentInput;
 }
 
+let result=0;
 //console.log(num2,num1,op);c
-equals.addEventListener("click", () => operate(op,parseInt(num1),parseInt(num2)));
+equals.addEventListener("click", () => { 
+    result=operate(op,parseInt(num1),parseInt(num2));
+    displayOutput(result);
+});
 
 //operate(op,num1,num2)
 
 function displayInput(e) {
     //    input.textContent=input.textContent+e;
     input.innerHTML=e;
+}
+
+function displayOutput(res) {
+    output.innerHTML=res;
 }
